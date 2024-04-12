8988cd2b410e28943f135ab347127454761b2a2b
ThanhLongNguyen08 <thanhlong0j@gmail.com>
2024-04-12 22:23:35 +0700

    update css home

---
+++ index.js
@@ -0,0 +1,22 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var slides = document.querySelectorAll('.slide');
+//     var currentSlide = 0;
+    
+//     // Hiển thị slide đầu tiên
+//     slides[currentSlide].classList.add('active');
+    
+//     function nextSlide() {
+//         // Ẩn slide hiện tại
+//         slides[currentSlide].classList.remove('active');
+        
+//         // Tăng chỉ số của slide hiện tại
+//         currentSlide = (currentSlide + 1) % slides.length;
+        
+//         // Hiển thị slide tiếp theo
+//         slides[currentSlide].classList.add('active');
+//     }
+    
+//     // Tự động chuyển slide sau mỗi 3 giây
+//     setInterval(nextSlide, 3000);
+// });
+
